fc239dd63ff01e07ea0b6d934340c751a21e979c
nitesh sharma <nitesh.grep@gmail.com>
2014-07-08 20:13:39 +0530

    adding list collection view

---
+++ js/app/list/list-collection-view.js
@@ -0,0 +1,16 @@
+define([ 'backbone', 'underscore', 'jquery'], function(Backbone, _, $) {
+
+	'use strict';
+
+	var ListCollectionView = Backbone.View.extend({
+		
+		el: '#list-view .list-container',
+
+		render: function(){
+
+		}
+	});
+
+	return ListCollectionView;
+
+});
