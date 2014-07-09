bb7d91cbc346b84aca36f0615b1ce29cfb0de82e
nitesh sharma <nitesh.grep@gmail.com>
2014-07-09 12:01:15 +0530

    adding add list view

---
+++ js/app/list/add-list-view.js
@@ -0,0 +1,26 @@
+define(['backbone', 'underscore', 'jquery'], function(Backbone, _, $) {
+
+	'use strict';
+
+	var AddListView = Backbone.View.extend({
+
+		el: '.add-new-list',
+
+		events: {
+			'keypress': "addListItem"
+		},
+
+		addListItem: function(e){
+			if(e.keyCode === 13){
+				var val = this.$el.val();
+				if(val){
+					this.collection.add({ title: val});
+				}
+				this.$el.val("");
+			}
+		}
+	});
+
+	return AddListView;
+
+});
