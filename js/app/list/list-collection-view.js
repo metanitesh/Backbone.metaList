5800666fd4a71bf17ce3881d48705623f08c3785
nitesh sharma <nitesh.grep@gmail.com>
2014-07-09 12:01:06 +0530

    flushing out list collection view

--- js/app/list/list-collection-view.js
+++ js/app/list/list-collection-view.js
@@ -1,13 +1,29 @@
-define([ 'backbone', 'underscore', 'jquery'], function(Backbone, _, $) {
+define(['backbone', 'underscore', 'jquery', 'ListModel', 'ListView'], function(Backbone, _, $, ListModel, ListView) {
 
 	'use strict';
 
 	var ListCollectionView = Backbone.View.extend({
-		
+
 		el: '#list-view .list-container',
 
-		render: function(){
+		initialize: function() {
+			this.render();
+			this.collection.on("add", this.addOne, this);
 
+		},
+
+		render: function() {
+			this.collection.each(function(list) {
+				this.addOne(list);
+			}, this);
+		},
+
+		addOne: function(list) {
+			var listView = new ListView({
+					model: list
+			});
+
+			this.$el.append(listView.el);
 		}
 	});
 
