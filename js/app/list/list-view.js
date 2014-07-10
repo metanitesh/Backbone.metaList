4fdcaac84914fa9260af5999e978ba446895c1c3
nitesh sharma <nitesh.grep@gmail.com>
2014-07-10 12:26:19 +0530

    updating listview

--- js/app/list/list-view.js
+++ js/app/list/list-view.js
@@ -1,4 +1,4 @@
-define(['backbone', 'underscore', 'jquery'], function(Backbone, _, $) {
+define(['TaskCollection', 'TaskCollectionView', 'AddTaskView', 'backbone', 'underscore', 'jquery'], function(TaskCollection, TaskCollectionView, AddTaskView, Backbone, _, $) {
 
 	'use strict';
 
@@ -18,14 +18,14 @@ define(['backbone', 'underscore', 'jquery'], function(Backbone, _, $) {
 		},
 
 
-		initialize: function(){
-			
+		initialize: function() {
+			// console.log(this.model)
 			this.model.on("destroy", this.remove, this);
 			this.model.on("change", this.reRender, this);
 			this.render();
 		},
 
-		activeTitle: function(e){
+		activeTitle: function(e) {
 
 			this.$el.siblings(".list").removeClass("list-active");
 			this.$el.siblings(".list").find(".input-wrapper").addClass("hidden");
@@ -37,39 +37,56 @@ define(['backbone', 'underscore', 'jquery'], function(Backbone, _, $) {
 			this.$el.addClass("list-active");
 			this.$el.find(".edit-list").addClass('icon-list-active');
 			this.$el.find(".delete-list").addClass('icon-delete-active');
+
+
+			
+				// console.log("listView active");
+				// var taskColletion = new TaskCollection(tasks);
+				// console.log(taskColletion);
+					
+				$(document).trigger("cli", this.model)
+				// var addTaskView = new AddTaskView({
+				// 	collection: this.model.tasks
+				// });
+
+				// console.log(addTaskView)
+			
 		},
 
-		adtiveEditView: function(){
+		adtiveEditView: function() {
 			this.$el.find(".input-wrapper").removeClass('hidden');
 			this.$el.find(".title").addClass('hidden');
 		},
 
-		update: function(e){
-			if(e.keyCode === 13){
+		update: function(e) {
+			if (e.keyCode === 13) {
 				var val = this.$el.find(".edit-list-input").val();
-				if(val){
+				if (val) {
 					this.model.set("title", val);
 				}
 			}
 
 		},
 
-		reRender: function(){
-			
+		reRender: function() {
+
 			this.$el.empty();
 			this.render();
 			this.activeTitle();
 		},
 
-		destroy: function(){
+		destroy: function() {
 			this.model.destroy();
 		},
 
-		remove: function(){
+		remove: function() {
 			this.$el.remove();
 		},
 
 		render: function() {
+
+
+
 			var temp = _.template(this.template);
 			var html = temp(this.model.toJSON());
 
