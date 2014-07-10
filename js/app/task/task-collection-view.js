7b2a7093edf7286745ba688b19316206826309ff
nitesh sharma <nitesh.grep@gmail.com>
2014-07-10 12:25:55 +0530

    updating collection view

--- js/app/task/task-collection-view.js
+++ js/app/task/task-collection-view.js
@@ -6,19 +6,26 @@ define(['backbone', 'underscore', 'jquery', 'TaskModel', 'TaskView'], function(B
 
 		el: '.task-group',
 
-		
-		initialize: function() {
-			
-			this.render();
 
-			this.collection.on("change", this.render, this);
-			this.collection.on("add", this.render, this);
+		initialize: function() {
+
+			$(document).on("cli", function(b, model) {
+				
+				this.collection = model.tasks;
+				this.render();
+
+				this.collection.on("add", this.render, this);
+				this.collection.on("change", this.render, this);
+
+			}.bind(this))
 
 		},
 
-		
+
 
 		render: function() {
+
+
 			this.$el.find('.task-complete').empty();
 			this.$el.find('.task-remaining').empty();
 			this.collection.each(function(task) {
@@ -29,11 +36,11 @@ define(['backbone', 'underscore', 'jquery', 'TaskModel', 'TaskView'], function(B
 		addOne: function(task) {
 
 			var taskView = new TaskView({
-					model: task
+				model: task
 			});
 
-			if(task.get("done")){
-				this.$el.find('.task-complete').append(taskView.el);	
+			if (task.get("done")) {
+				this.$el.find('.task-complete').append(taskView.el);
 			} else {
 				this.$el.find('.task-remaining').append(taskView.el);
 			}
