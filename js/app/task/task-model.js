d29acbc0805587fb10a225fdcd7e1a5a1f9b092a
nitesh sharma <nitesh.grep@gmail.com>
2014-07-11 20:35:45 +0530

    refactoring task model

--- js/app/task/task-model.js
+++ js/app/task/task-model.js
@@ -1,9 +1,22 @@
-define(['backbone'], function(Backbone){
-	
+define(['backbone'], function(Backbone) {
+
 	"use strict";
-	
-	var TaskModel = Backbone.Model.extend({});
-		 
+
+	var TaskModel = Backbone.Model.extend({
+
+		defaults: {
+			content: '',
+			comments: [],
+			done: false
+		},
+
+		validate: function(attrs) {
+			if (!attrs.title.replace(/^\s+|\s+$/g, '')) {
+				return "task must have title";
+			}
+		}
+	});
+
 	return TaskModel;
 
 });
