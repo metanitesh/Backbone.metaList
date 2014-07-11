ff1664e41ef960a8533021a7090c979ba3234199
nitesh sharma <nitesh.grep@gmail.com>
2014-07-11 20:10:31 +0530

    adding ListCollection spec

---
+++ tests/unit/spec/listcollectionspec.js
@@ -0,0 +1,32 @@
+define(['ListCollection'], function(ListCollection) {
+
+	describe('ListCollection', function() {
+
+		it('is sane', function() {
+
+			/*given*/
+			var listCollection = new ListCollection();
+
+			/*then*/
+			expect(typeof listCollection).toEqual('object');
+		});
+
+		it('can store listmodels', function() {
+
+			/*given*/
+			listCollection = new ListCollection();
+
+			/*when*/
+			listCollection.add([{
+				title: "books"
+			}, {
+				title: "movie"
+			}]);
+
+			/*then*/
+			expect(listCollection.at(0).get('title')).toEqual("books");
+			expect(listCollection.at(1).get('title')).toEqual("movie");
+		})	
+	});
+
+});
