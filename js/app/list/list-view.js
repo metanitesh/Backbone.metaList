29ce094f9195fb8d726d76e6bc1528d7d5650ccc
nitesh sharma <nitesh.grep@gmail.com>
2014-07-11 20:11:12 +0530

    refactoring listView

--- js/app/list/list-view.js
+++ js/app/list/list-view.js
@@ -11,87 +11,78 @@ define(['TaskCollection', 'TaskCollectionView', 'AddTaskView', 'backbone', 'unde
 		template: $('#list-item').html(),
 
 		events: {
-			'click': 'activeTitle',
-			'dblclick .title': 'adtiveEditView',
+			'click': 'select',
+			'dblclick .title': 'EditView',
 			'click .delete-list': 'destroy',
 			'keypress .edit-list-input': 'update',
 		},
 
 
 		initialize: function() {
-			// console.log(this.model)
 			this.model.on("destroy", this.remove, this);
-			this.model.on("change", this.reRender, this);
+			this.model.on("change", this.updateView, this);
+			this.model.on("invalid", this.showError, this);
 			this.render();
 		},
 
-		activeTitle: function(e) {
 
+		resetAllListStyle: function() {
 			this.$el.siblings(".list").removeClass("list-active");
 			this.$el.siblings(".list").find(".input-wrapper").addClass("hidden");
 			this.$el.siblings(".list").find(".title").removeClass('hidden');
 
 			this.$el.siblings(".list").find(".icon-list").removeClass('icon-list-active');
 			this.$el.siblings(".list").find(".delete-list").removeClass('icon-delete-active');
+		},
 
+		activeListStyle: function() {
 			this.$el.addClass("list-active");
 			this.$el.find(".edit-list").addClass('icon-list-active');
 			this.$el.find(".delete-list").addClass('icon-delete-active');
-
-
-			
-				// console.log("listView active");
-				// var taskColletion = new TaskCollection(tasks);
-				// console.log(taskColletion);
-					
-				$(document).trigger("cli", this.model)
-				// var addTaskView = new AddTaskView({
-				// 	collection: this.model.tasks
-				// });
-
-				// console.log(addTaskView)
-			
 		},
 
-		adtiveEditView: function() {
+
+		EditView: function() {
 			this.$el.find(".input-wrapper").removeClass('hidden');
 			this.$el.find(".title").addClass('hidden');
+		},
+
+		select: function(e) {
+			this.resetAllListStyle();
+			this.activeListStyle();
+			$(document).trigger("cli", this.model);
 		},
 
 		update: function(e) {
 			if (e.keyCode === 13) {
 				var val = this.$el.find(".edit-list-input").val();
 				if (val) {
-					this.model.set("title", val);
+					this.model.set("title", val, {validate: true});
 				}
 			}
-
 		},
 
-		reRender: function() {
-
+		updateView: function() {
 			this.$el.empty();
 			this.render();
-			this.activeTitle();
+			this.select();
 		},
 
 		destroy: function() {
 			this.model.destroy();
 		},
 
-		remove: function() {
-			this.$el.remove();
-		},
 
 		render: function() {
-
-
-
-			var temp = _.template(this.template);
-			var html = temp(this.model.toJSON());
+			var compiled = _.template(this.template);
+			var html = compiled(this.model.toJSON());
 
 			this.$el.append(html);
 			return this;
+		},
+
+		showError: function(model, error){
+			alert(error);
 		}
 	});
 
