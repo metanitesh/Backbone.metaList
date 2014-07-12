a83c9dcde9bf13c8eaa0b74657a27089719e7c69
nitesh sharma <nitesh.grep@gmail.com>
2014-07-12 19:25:57 +0530

    adding bootstrapdata

---
+++ js/app/bootstrapdata.js
@@ -0,0 +1,46 @@
+define([], function() {
+	var data = [{
+		title: "to-do",
+		tasks: [{
+			"title": "clean house",
+			"comments": ["after lunch", "maybe tomorrow", "doesn't look that bad"],
+			"content": "it's been ages man !",
+			"done": false
+		}, {
+			"title": "get a haircut",
+			"comments": ["will do this weekend"],
+			"content": "this could be a good reason to become a monk",
+			"done": true
+		}, {
+			"title": "buy grocery",
+			"comments": [],
+			"content": "1. eggs\n2. apples\n3. bread \n4. wine",
+			"done": false
+		}]
+	}, {
+		title: "books",
+		tasks: [{
+			"title": "The Art Spirit",
+			"comments": ["a work of art which inspires comes from no quibbling or uncertain man", "the book is pure delight for detail oriented peoples"],
+			"content": "In certain books- some way in the first few\nparagraphs you know that you have met a brother.",
+			"done": false
+		}, {
+			"title": "The Alchemist",
+			"comments": ["how to make a great book even better - make a graphic novel"],
+			"content": "It's simple things in life that are most extraordinary. only wise man are able to understand them.\n",
+			"done": true
+		}, {
+			"title": "The Noticer",
+			"comments": ["get some perspective from an old man"],
+			"content": "If one makes a mistake then an apology is usually sufficient to get things back on keel. However most peoples doest not seems to understand why  their apology have no effect. it is simply because they did not make a mistake. they made a choice.\n\n",
+			"done": true
+		}, {
+			"title": "Essentialism",
+			"comments": [],
+			"content": "We often think of choice as a thing. But choice is not a thing. our option may be  things. but a choice- a choice is an action.",
+			"done": false
+		}]
+	}];
+
+	return data;
+});
