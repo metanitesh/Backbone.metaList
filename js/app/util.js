4ec5fb8cdf4d1a8d6b4f0eaf00860e166f99bf62
nitesh sharma <nitesh.grep@gmail.com>
2014-07-15 22:20:17 +0530

    adding util.js

---
+++ js/app/util.js
@@ -0,0 +1,44 @@
+define(["backbone"], function(Backbone) {
+	
+	var util = {
+		vent: _.extend(Backbone.Events),
+		
+		imageLoader: function(imageArr) {
+
+			var loadedImages = 0;
+			var images = [];
+			var postAction = function() {};
+			var postLoad = function() {
+				loadedImages++;
+				if (loadedImages === imageArr.length) {
+					postAction(images);
+				}
+			};
+
+			var success = function() {
+				postLoad();
+			};
+
+			var fail = function() {
+				postLoad();
+			};
+			for (var i = 0; i < imageArr.length; i++) {
+				var src = "images/" + imageArr[i];
+				images[i] = new Image();
+				images[i].src = src;
+				images[i].onload = success;
+				images[i].onError = fail;
+
+			}
+
+			return {
+				done: function(f) {
+					postAction = f || postAction;
+				}
+			};
+		}
+	};
+
+
+	return util;
+});
