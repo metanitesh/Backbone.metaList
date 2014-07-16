569e29ea26bcf2b981a0ea8e010ddd23b3ece409
nitesh sharma <nitesh.grep@gmail.com>
2014-07-16 18:40:22 +0530

    updating app.js

--- js/app/app.js
+++ js/app/app.js
@@ -5,9 +5,9 @@ define(['jquery', 'underscore', 'app', 'util'], function($, _, APP, util) {
 	$(function() {
 		var preloadImages = ["icon-sprite.png", "detail.png", "list-active.jpg", "logo_bg.png", "task.png", "sidebar.png", "tweed.png"];
 		util.imageLoader(preloadImages).done(function() {
-
+			$(".loader").hide();
 			window.APP = APP.init();
-
+			
 		});
 	});
 
