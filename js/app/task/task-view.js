c0033d1e5d8d9e5d23541dda284964dcfd723ff8
nitesh sharma <nitesh.grep@gmail.com>
2014-07-16 18:41:40 +0530

    updating taskview

--- js/app/task/task-view.js
+++ js/app/task/task-view.js
@@ -8,7 +8,7 @@ define(['backbone', 'underscore', 'jquery'], function(Backbone, _, $) {
 
 		className: 'task-item',
 
-		template : $("#task-item").html(),
+		template: $("#task-item").html(),
 
 		events: {
 			"click .check-task": "checkOff",
@@ -16,33 +16,36 @@ define(['backbone', 'underscore', 'jquery'], function(Backbone, _, $) {
 			"click": "select"
 		},
 
-		initialize: function(){
+		initialize: function() {
+			this.model._events.destroy = [];
 			this.listenTo(this.model, "destroy", this.remove, this);
 			this.render();
 		},
 
-		activeTaskState: function(){
+		activeTaskState: function() {
 			this.$el.closest(".task-group").find("li").removeClass("task-item-selected");
 			this.$el.addClass("task-item-selected");
 		},
-		
 
-		select: function(){
-			var listId  = this.model.collection.parent.get("id");
-			APP.router.navigate(listId +"/"+this.model.get("id"), { trigger: true});
+
+		select: function() {
+			var listId = this.model.collection.parent.get("id");
+			APP.router.navigate(listId + "/" + this.model.get("id"), {
+				trigger: true
+			});
 		},
 
-		checkOff: function(){
+		checkOff: function() {
 			this.model.set("done", true);
 		},
 
-		destroy: function(e){
+		destroy: function(e) {
 			e.preventDefault();
 			e.stopPropagation();
 			this.model.destroy();
 		},
 
-		render: function(){
+		render: function() {
 			var compiled = _.template(this.template);
 			var html = compiled(this.model.toJSON());
 			this.$el.append(html);
